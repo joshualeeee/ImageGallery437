8fe8be3d088ad00b166db916c02a867f866a7c3e
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2025-05-26 15:04:26 -0700

    links

--- packages/frontend/src/Header.tsx
+++ packages/frontend/src/Header.tsx
@@ -1,19 +1,20 @@
 import "./Header.css";
+import { Link } from "react-router";
 
 export function Header() {
-    return (
-        <header>
-            <h1>My cool image site</h1>
-            <div>
-                <label>
-                    Some switch (dark mode?) <input type="checkbox" />
-                </label>
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/upload">Upload</a>
-                    <a href="/login">Log in</a>
-                </nav>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <h1>My cool image site</h1>
+      <div>
+        <label>
+          Some switch (dark mode?) <input type="checkbox" />
+        </label>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/upload">Upload</Link>
+          <Link to="/login">Log in</Link>
+        </nav>
+      </div>
+    </header>
+  );
 }
--- packages/frontend/src/images/ImageDetails.tsx
+++ packages/frontend/src/images/ImageDetails.tsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
 import { MainLayout } from "../MainLayout.tsx";
 import { fetchDataFromServer } from "../MockAppData.ts";
+import { useParams } from "react-router";
 
-interface IImageDetailsProps {
-    imageId: string;
-}
+// interface IImageDetailsProps {
+//     imageId: string;
+// }
 
-export function ImageDetails({ imageId }: IImageDetailsProps) {
-    const [imageData, _setImageData] = useState(fetchDataFromServer);
-    const image = imageData.find(image => image.id === imageId);
-    if (!image) {
-        return <MainLayout><h2>Image not found</h2></MainLayout>;
-    }
-
+export function ImageDetails() {
+  const [imageData, _setImageData] = useState(fetchDataFromServer);
+  const { imageId } = useParams();
+  const image = imageData.find((image) => image.id === imageId);
+  if (!image) {
     return (
-        <MainLayout>
-            <h2>{image.name}</h2>
-            <p>By {image.author.username}</p>
-            <img className="ImageDetails-img" src={image.src} alt={image.name} />
-        </MainLayout>
-    )
+      <MainLayout>
+        <h2>Image not found</h2>
+      </MainLayout>
+    );
+  }
+
+  return (
+    <MainLayout>
+      <h2>{image.name}</h2>
+      <p>By {image.author.username}</p>
+      <img className="ImageDetails-img" src={image.src} alt={image.name} />
+    </MainLayout>
+  );
 }
