b09b9c6140d4aed4f6feeb9af0a08c516c7a21d9
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2025-05-26 16:21:21 -0700

    add error and loading

--- packages/frontend/src/images/AllImages.tsx
+++ packages/frontend/src/images/AllImages.tsx
@@ -1,11 +1,33 @@
 import { ImageGrid } from "./ImageGrid.tsx";
-import type { IImageData } from "../MockAppData.ts";
+import type { IApiImageData } from "csc437-monorepo-backend/src/common/ApiImageData";
 
 interface AllImagesProps {
-  images: IImageData[];
+  images: IApiImageData[];
+  isLoading: boolean;
+  hasError: boolean;
 }
 
-export function AllImages({ images }: AllImagesProps) {
+export function AllImages({ images, isLoading, hasError }: AllImagesProps) {
+  if (isLoading) {
+    return (
+      <>
+        <h2>All Images</h2>
+        <div>Loading images...</div>
+      </>
+    );
+  }
+
+  if (hasError) {
+    return (
+      <>
+        <h2>All Images</h2>
+        <div style={{ color: "red" }}>
+          Error loading images. Please try again later.
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <h2>All Images</h2>
