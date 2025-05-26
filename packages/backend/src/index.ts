fb7aadac5d26ebb03322b91b5f26ab88adf5718a
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2025-05-26 16:20:19 -0700

    add images endpoint

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { ValidRoutes } from "./shared/ValidRoutes";
+import { IMAGES } from "./common/ApiImageData";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
@@ -10,8 +11,12 @@ const STATIC_DIR = process.env.STATIC_DIR || "public";
 const app = express();
 app.use(express.static(STATIC_DIR));
 
-app.get("/hello", (req: Request, res: Response) => {
+app.get("/api/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
+});
+
+app.get("/api/images", (req: Request, res: Response) => {
+    res.json(IMAGES);
 });
 
 // Handle all valid routes by serving index.html
